89287c79cfcc5c5deaa8668c03a510d99372505a
rod773 <rodrigoarcadio1@gmail.com>
2024-08-25 18:06:10 +0200

    update

---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- saved from url=(0040)https://plataformadelmayor.org/sendmail/ -->
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+
+<body>
+    <h1>Hola Mundo</h1>
+</body>
+
+</html>
