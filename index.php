498eadd6baacd7c09f17db6023d37c79c2e4d52c
rod773 <rodrigoarcadio1@gmail.com>
2024-08-25 17:46:58 +0200

    update

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
+<body>
+    <h1>Hola Mundo</h1>
+</body>
 
-echo "<h1>Hola Mundo</h1>";
+</html>
